a3e8e23c634f93adfcfb6bfb247f548275e02bfa
21ntphilos <ucheokoye99@gmail.com>
2025-10-08 16:48:21 +0100

    added notification

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.core.mail import send_mail
 
 from django.contrib.auth import get_user_model
 
@@ -17,11 +18,20 @@ class UserAccountSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
+
         user = get_user_model().objects.create_user(
             email=validated_data['email'],
             full_name=validated_data['full_name'],
             password=validated_data['password']
         )
-       
+
+        send_mail(
+            subject='Welcome to Our APP',
+            message=f'Hi {user.full_name}, thank you for registering at our platform.',
+            from_email='zerosandone01@gmail.com',
+            fail_silently=False,
+            recipient_list=[user.email],
+        )
+
         return user
     
--- core/settings.py
+++ core/settings.py
@@ -134,3 +134,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
+
+
+# EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+# SENDGRID_API_KEY = config("SENDGRID_API_KEY")
+# DEFAULT_FROM_EMAIL = config("FROM_EMAIL")
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL-USER')
+EMAIL_HOST_PASSWORD = config('EMAIL-PASSWORD') 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
